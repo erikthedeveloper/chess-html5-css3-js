fa75a8d51e50b676e644052a9ef80f839786c04e
Dev on CentosDev <erikthedeveloper@gmail.com>
2014-01-17 16:30:35 +0000

    Added Disqus, .chessboard-wrapper, and link to Github

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Chess'd - An Open Sourced HTML5, CSS3, & JavaScript Online Chess Game</title>
-    
+
     <meta name="description" content="Open source online chess game built with HTML5, CSS3, and JavaScript. Drag & Drop with HTML5 elements, CSS3 transforms, and JavaScript. An Erik Aybar Creation" >
     <!-- Image Credit to miroha at alphacoders.com (http://wall.alphacoders.com/big.php?i=409614) -->
     <link href='http://fonts.googleapis.com/css?family=Josefin+Sans:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic' rel='stylesheet' type='text/css'>
@@ -44,23 +44,44 @@ $rows = array(
     );
 ?>
 
-<h1>HTML5 & CSS3 Chessboard</h1>
 
-    <div class="chessboard" id="chessboard">
+    <div class="chessboard-wrapper">
+        <h1>Chess'd - HTML5, CSS3, and JavaScript Online Chessboard</h1>
+	<p><a href="https://github.com/erikthedeveloper/chess-html5-css3-js" title="I'm open source" target="_blank">View me on Github</a></p>
+        <div class="chessboard" id="chessboard">
 
-        <?php
-        foreach ($rows as $row): ?>
-            <div class="row">
-            <?php foreach ($row as $col):
-            $is_white = preg_match('/w-/', $col);
-            $piece_class = "piece";
-            $piece_class .= $is_white ? " white" : " black";
-            ?>
-                <div class="square draggable" draggable="true"><div class="<?php echo $piece_class; ?>"><?php echo $pieces[$col]; ?></div></div>
+            <?php
+            foreach ($rows as $row): ?>
+                <div class="row">
+                <?php foreach ($row as $col):
+                $is_white = preg_match('/w-/', $col);
+                $piece_class = "piece";
+                $piece_class .= $is_white ? " white" : " black";
+                ?>
+                    <div class="square draggable" draggable="true"><div class="<?php echo $piece_class; ?>"><?php echo $pieces[$col]; ?></div></div>
+                <?php endforeach ?>
+                </div>
             <?php endforeach ?>
-            </div>
-        <?php endforeach ?>
 
+        </div>
+    </div>
+
+    <div class="disqus_wrapper">
+        <h4>Enjoy Chess'd? Let me know below</h4>
+        <div id="disqus_thread"></div>
+        <script type="text/javascript">
+            /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+            var disqus_shortname = 'erikaybar'; // required: replace example with your forum shortname
+
+            /* * * DON'T EDIT BELOW THIS LINE * * */
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
     </div>
 
     <script src="main.js"> </script>
